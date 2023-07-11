e1a68c9761d35d5bfd4fda6dba195caf1796e99a
pineroya <yamil.rokr@gmail.com>
2023-07-11 11:41:44 -0300

    configuracion postgreSQL en railway.app

--- Tiendacompleta/settings.py
+++ Tiendacompleta/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as mensajes_de_error
 from django.contrib.staticfiles import *
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,6 +98,8 @@ DATABASES = {
         'PORT': '5432',
         }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
